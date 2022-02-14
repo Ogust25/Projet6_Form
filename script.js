7c906ee59f91235c17281dc11d3b315694ba49c1
Ogu <august.gros@gmail.com>
2022-02-14 17:01:47 +0100

    [Add]
    ajax non fonctionnel

--- script.js
+++ script.js
@@ -8,7 +8,16 @@ const regleNomPrenom = /^[a-zA-Záàâäãåçéèêëíìîïñóòôöõúùû
 const regleEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 const regleMessage = /^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ',;()-]+$/;
 
-/* btn.addEventListener('click', function(){
+btn.addEventListener('click', function(e){
+    e.preventDefault()
+    fetch('traitement.php',{method: 'POST'})
+    .then((response) =>
+        response.json())
+        .then((data) => {
+        console.log(data);
+        document.querySelector('.txtValider').innerHTML = data.validation;
+    })
+
     let resultatNom = regleNomPrenom.test(inputNom.value)
     let resultatPrenom = regleNomPrenom.test(inputPrenom.value)
     let resultatEmail = regleEmail.test(inputEmail.value)
@@ -28,41 +37,4 @@ const regleMessage = /^[a-zA-Záàâäãåçéèêëíìîïñóòôöõúùûü
     if(resultatEmail == false){
         document.querySelector('.emailError').style.visibility = "visible";
     }
-}) */
-
-
-/* AJAX */
-
-btn.addEventListener('submit', function() {
-    httpRequest = new XMLHttpRequest();
-    httpRequest.onreadystatechange = function() {
-        if (httpRequest.readyState == 4) {
-            if (httpRequest.status == 200) {
-                let resultatNom = regleNomPrenom.test(inputNom.value)
-                let resultatPrenom = regleNomPrenom.test(inputPrenom.value)
-                let resultatEmail = regleEmail.test(inputEmail.value)
-
-                if((resultatNom == true)&&(resultatPrenom == true)&&(resultatEmail == true)){
-                    document.querySelector('.txtValider').style.visibility = "visible";
-                    document.querySelector('.nomError').style.visibility = "hidden";
-                    document.querySelector('.prenomError').style.visibility = "hidden";
-                    document.querySelector('.emailError').style.visibility = "hidden";
-                }
-                if(resultatNom == false){
-                    document.querySelector('.nomError').style.visibility = "visible";
-                }
-                if(resultatPrenom == false){
-                    document.querySelector('.prenomError').style.visibility = "visible";
-                }
-                if(resultatEmail == false){
-                    document.querySelector('.emailError').style.visibility = "visible";
-                }
-            } else {
-                alert('Il y a eu un problème avec la requête.');
-            }
-        }
-    }
-    httpRequest.open('POST', 'traitement.php?submit=true');
-    httpRequest.send();
 })
-
--- traitement.php
+++ traitement.php
@@ -1,26 +1,32 @@
 <?php
-
 $regleNomPrenom = "/^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ'-]+$/";
 $regleEmail = "/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/";
 $regleMessage = "/^[a-zA-ZáàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ',;()-]+$/";
 
-if((((isset($_POST['nom']))||(empty($_POST['nom']))))&&(preg_match($regleNomPrenom, $_POST['nom']))){
+if (isset($_POST['nom'])){
     $nom = $_POST['nom'];
+    $res = array('validation' => "Email envoyé");
+    echo json_encode($res);
 }
-if((((isset($_POST['prenom']))||(empty($_POST['prenom']))))&&(preg_match($regleNomPrenom, $_POST['prenom']))){
+/* if((((isset($_POST['nom']))||(!empty($_POST['nom'])){
+    $nom = $_POST['nom'];
+    echo json_encode($nom);
+} */
+/* if((((isset($_POST['prenom']))||(!empty($_POST['prenom']))))&&(preg_match($regleNomPrenom, $_POST['prenom']))){
     $prenom = $_POST['prenom'];
 }
-if((((isset($_POST['email']))||(empty($_POST['email']))))&&(preg_match($regleEmail, $_POST['email']))){
+if((((isset($_POST['email']))||(!empty($_POST['email']))))&&(preg_match($regleEmail, $_POST['email']))){
     $email = $_POST['email'];
 }
-if(((isset($_POST['message']))||(empty($_POST['message'])))&&(preg_match($regleMessage, $_POST['message']))){
+if(((isset($_POST['message']))||(!empty($_POST['message'])))&&(preg_match($regleMessage, $_POST['message']))){
     $message = $_POST['message'];
-}
-
-if(($nom == true)&&($prenom == true)&&($email == true)&&($message == true)){
+} */
+/* 
+if($nom){
+    $res = array('validation' => "Email envoyé");
+    echo json_encode($res);
     mail("sxxasif@leadwizzer.com", "Bonjour, $prenom", $message, "From: $email");
-}
+} */
 
-header('Location: validation.html');
 
 ?>
